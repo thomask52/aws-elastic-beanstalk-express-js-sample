4088589306a0b8008face6928fc041ec5f56c636
TomA <69837372+thomask52@users.noreply.github.com>
2021-02-25 16:13:33 +0800

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('yo yo yo!'));
+app.get('/', (req, res) => res.send('ho ho ho!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
